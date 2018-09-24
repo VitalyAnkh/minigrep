95621a6fb3bb8ffd192f904866f56c4291287263
VitalyR <vitalyankh@gmail.com>
2018-09-25 02:59:43 +0800

    improve modularity

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use std::env;
-use std::fs::File;
 use std::io::prelude::*;
 use std::process;
+extern crate minigrep;
+
+use minigrep::Config;
+
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -13,31 +16,9 @@ fn main() {
 
     println!("searching for {}", config.query);
     println!("in file {}", config.filename);
-
-    let mut f = File::open(config.filename).expect("file not found!");
-
-    let mut contents = String::new();
-    f.read_to_string(&mut contents)
-        .expect("something went wrong reading the file!");
-    println!("with text:");
-    println!("{}", contents);
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-        Ok(Config {
-            query,
-            filename,
-        })
+    if let Err(e) = minigrep::run(config){
+        println!("Application error: {}",e);
+        process::exit(1);
     }
 }
+
