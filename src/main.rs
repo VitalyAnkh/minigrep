79fcee4aa740cf45a81ecb6826965eb3aaaed447
vitalyankh <vitalyankh@gmail.com>
2018-09-24 13:32:47 +0800

    refactor project, extracting the arguments parser and grouping the configuration values

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,30 @@ use std::io::prelude::*;
 
 fn main(){
     let args:Vec<String>=env::args().collect();
-    let query = &args[1];
-    let filename=&args[2];
+    let config= parse_config(&args);
 
-    println!("searching for {}",query);
-    println!("in file {}",filename);
+    println!("searching for {}",config.query);
+    println!("in file {}",config.filename);
 
-    let mut f = File::open(filename).expect("file not found!");
+    let mut f = File::open(config.filename).expect("file not found!");
 
     let mut contents=String::new();
     f.read_to_string(&mut contents)
         .expect("something went wrong reading the file!");
     println!("with text:");
     println!("{}",contents);
+}
+
+struct Config {
+    query:String,
+    filename:String,
+}
+
+fn parse_config(args:&[String])->Config{
+    let mut query = args[1].clone();
+    let mut filename = args[2].clone();
+    Config{
+        query,
+        filename
+    }
 }
