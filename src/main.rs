d6e6794906a7912c4a360b76d06e4c8d6ffe49da
vitalyankh <vitalyankh@gmail.com>
2018-09-24 13:07:14 +0800

    reading file and 红楼梦.ttxt

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
+fn main(){
+    let args:Vec<String>=env::args().collect();
+    let query = &args[1];
+    let filename=&args[2];
+
+    println!("searching for {}",query);
+    println!("in file {}",filename);
+
+    let mut f = File::open(filename).expect("file not found!");
+
+    let mut contents=String::new();
+    f.read_to_string(&mut contents)
+        .expect("something went wrong reading the file!");
+    println!("with text:");
+    println!("{}",contents);
 }
