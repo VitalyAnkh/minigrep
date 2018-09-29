6f6d5810dd419858b7ab0d04c0e17b9d0a4c33d7
VitalyR <vitalyankh@gmail.com>
2018-09-29 23:58:37 +0800

    finish TDD

--- src/lib.rs
+++ src/lib.rs
@@ -35,9 +35,9 @@ mod test {
     fn one_result() {
     let query="duct";
     let contents="\
-    rust:\
-    safe, fast, productive.\
-    Pick three.";
+rust:
+safe, fast, productive.
+Pick three.";
     assert_eq!(vec!["safe, fast, productive."],
     search(query,contents)
     )
@@ -45,5 +45,11 @@ mod test {
 }
 
 pub fn search<'a>(query:&str, contents:&'a str)->Vec<&'a str>{
-    vec![]
+    let mut result=Vec::new();
+    for line in contents.lines(){
+        if line.contains(query){
+          result.push(line);
+        }
+    }
+    result
 }
